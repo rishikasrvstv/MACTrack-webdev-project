140009c37469bbc82f85f8b26f2aa83ffec0e9ce
rishikasrvstv <167618469+rishikasrvstv@users.noreply.github.com>
2024-04-28 23:31:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+menuIcon.oneclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    menuIcon.classList.toggle('active');
+};
+let sections= document.querySelectorAll('section');
+let navLinks= document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    section.forEach(sec=> {
+        let top=window.scrollY;
+        let offset=sec.offsetTop-150;
+        let height=sec.offsetHeight;
+        let id =sec.getAttribute('id');
+
+        if(top>=offset && top<offset + height){
+            navLinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
+            });
+        };
+    });
+
+let header=document.querySelector('header');
+header.classList.toggle('sticky',window.scrollY > 100);
+
+menuIcon.classList.toggle('bx-x');
+navbar.classList.toggle('active');
+
+};
+
+
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration:2000,
+    delay:200
+});
+ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
+ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact form',{origin:'bottom'});
+ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
+
+const typed =new Typed('.multiple-text',{
+    strings:['Frontend Developer','Content Writer','Freelancer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
